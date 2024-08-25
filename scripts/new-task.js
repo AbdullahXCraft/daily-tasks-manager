59235880fdcb167d61ff06efabcd008574978bab
abdullah <aboodi_salem1@outlook.com>
2024-08-25 15:59:20 +0300

    Complete the ui for new-task.html

--- scripts/new-task.js
+++ scripts/new-task.js
@@ -1,3 +1,13 @@
 $(function () {
-  $('.task-repeat').selectmenu();
+  $('.task-repeat').selectmenu({
+    icons: { 
+      button: "ui-icon-caret-1-s" 
+    },
+    open: (event) => {
+      $(event.target).selectmenu( "option", "icons", { button: "ui-icon-caret-1-n" } );
+    },
+    close: (event) => {
+      $(event.target).selectmenu( "option", "icons", { button: "ui-icon-caret-1-s" } );
+    }
+  });
 });
